a3b8b754867bcf636d46aa6beab29831235b2af5
hyuntaeeom <hyuntaeeom@hyuntaeeomui-MacBookPro.local>
2019-07-24 01:38:12 +0900

    [1-4] Add ant-design and AppLayout components and Head

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,13 +1,21 @@
 import React from 'react'; // eslint 에서 react 쓰면 import 하라고 명시됨
 import Link from 'next/link';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
   return (
     <>
-      <Link href="/about"><a>about</a></Link>
-      <div>
-        Hello, Next!
-      </div>
+      <Head>
+        <title>NodeBird</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.20.5/antd.css"/>
+      </Head>
+      <AppLayout>
+        <Link href="/about"><a>about</a></Link>
+        <div>
+          Hello, Next!
+        </div>
+      </AppLayout>
     </>
   );
 };
