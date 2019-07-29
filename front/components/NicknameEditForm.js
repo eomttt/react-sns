f87bf10adb45b2e645959e1d197c0642c1717b7a
hyuntaeeom <hyuntaeeom@hyuntaeeomui-MacBookPro.local>
2019-07-30 00:47:12 +0900

    [2-6, 7] Add profile pages

---
+++ front/components/NicknameEditForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Form, Input, Button } from 'antd'
+
+const NicknameEditForm = () => {
+	return (
+    <Form style={{ marginTop: '20px'}}>
+      <Input style={{ marginBottom: '10px' }} addonBefore="닉네임"/>
+      <Button type="primary">Change</Button>
+    </Form>
+	)
+};
+
+export default NicknameEditForm;
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { List, Card, Icon, Button } from 'antd';
+
+import NicknameEditForm from '../components/NicknameEditForm';
 
 const Profile = () => {
   return (
     <>
-      <div>
-        Profile!
-      </div>
+      <NicknameEditForm/>
+      <List
+        style={{ marginTop: '20px' }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>Following</div>}
+        dataSource={['Following_1', 'Following_2', 'Following_3']}
+        loadMore={<Button style={{ width: '100%' }}>LoadMore</Button>}
+        bordered
+        renderItem={item => (
+          <List.Item style={{ marginTop: '20px' }}>
+            <Card actions={[<Icon key="stop" type="stop" />]}><Card.Meta description={item} /></Card>
+          </List.Item>
+        )}
+      />
+      <List
+        style={{ marginTop: '20px' }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        header={<div>Follower</div>}
+        dataSource={['Follower_1', 'Follower_2', 'Follower_3']}
+        loadMore={<Button style={{ width: '100%' }}>LoadMore</Button>}
+        bordered
+        renderItem={item => (
+          <List.Item style={{ marginTop: '20px' }}>
+            <Card actions={[<Icon key="stop" type="stop" />]}><Card.Meta description={item} /></Card>
+          </List.Item>
+        )}
+      />
     </>
   );
 };
