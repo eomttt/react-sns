9c7c3076bf3cd7c00960e960767234c5767ac6fb
Eomttt <hyunt0413@gmail.com>
2019-07-30 19:41:07 +0900

    [3-4] Linked redux in react

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
-import AppLayout from '../components/AppLayout';
+import withRedux from 'next-redux-wrapper'; // With redux in next
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux'
 
-const NodeBird = ({ Component }) => {
+import reducers from '../reducers';
+import AppLayout from '../components/AppLayout';
+import { initialState } from '../reducers/user';
+
+const NodeBird = ({ Component, store }) => {
+  // Using Provider link react with redux
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>NodeBird</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.20.5/antd.css"/>
@@ -13,12 +20,22 @@ const NodeBird = ({ Component }) => {
       <AppLayout>
         <Component/>
       </AppLayout>
-    </>
+    </Provider>
   );
 };
 
 NodeBird.propTypes = {
-  Component: PropTypes.elementType
+  Component: PropTypes.elementType,
+  store: PropTypes.object
 }
 
-export default NodeBird;
+export default withRedux((initialState, options) => {
+  const middlewares = [];
+  const enhancer = compose(
+    applyMiddleware(...middlewares),
+    !options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f, // For debuggin redux
+  );
+
+  const store = createStore(reducers, initialState, enhancer); // Create redux store
+  return store;
+})(NodeBird);
